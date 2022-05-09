042b8137b8e3bb374b7b92ca5d849f8202b9e17a
Suraj <kumar.suraj@rwit.io>
2022-05-09 14:53:54 +0530

    config file changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,37 @@
-const { lineHeight } = require('tailwindcss/defaulttheme');
 
 module.exports = {
   content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}",
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}'
   ],
-  
   theme: {
     fontFamily: {
       sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
+      serif: ['Merriweather', 'serif']
     },
     minHeight: {
-      'img-height': '230px',
+      'img-height': '230px'
     },
-    
     extend: {
       backgroundImage: {
-        'header-img': "url('~/assets/img/film-70638.jpg')",
+        'header-img': "url('~/assets/img/film-70638.jpg')"
       },
-      
       gridTemplateColumns: {
-        'header' : '500px 2fr',
+        header: '500px 2fr'
       },
 
       lineHeight: {
-        '12':'4rem',
+        12: '4rem'
       }
     },
     color: {
-      'midnight': '#f0f8ff',
+      midnight: '#f0f8ff'
     }
   },
   plugins: [
-    require('@tailwindcss/forms'),
-  ],
+    require('@tailwindcss/forms')
+  ]
 }
